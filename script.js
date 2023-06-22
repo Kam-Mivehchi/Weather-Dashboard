dee11c108eff2b761a7ced701b49350d9b56a054
kmivehchi-xRoird <kammivehchi@gmail.com>
2023-06-21 22:27:38 -0700

    get weather data on the page, all that is left is optimization and styling

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const forecastCard = document.getElementById("forecast")
 const LocalStorageUtil = {
     setItem(key, value) {
         try {
-            if(key==="units"){
+            if (key === "units") {
                 localStorage.setItem(key, JSON.stringify(value))
                 return;
             }
@@ -46,77 +46,136 @@ const LocalStorageUtil = {
 init()
 
 
-document.getElementById("search-form").addEventListener("submit",handleSearch);
+document.getElementById("search-form").addEventListener("submit", handleSearch);
 
-function handleSearch(e){
+function handleSearch(e) {
     e.preventDefault();
-    console.dir(e.target[1].checked)
+    // console.dir(e.target[1].checked)
 
-    
+
     //fetches data from openweather then generate current weather card
-    fetchCurrentWeather(e.target[0].value.trim()) 
-
+    fetchCurrentWeather(e.target[0].value.trim())
+    fetchForecast(e.target[0].value.trim())
     //reset the input
-    searchInput.value=""
+    searchInput.value = ""
 }
 
 
 //creates a url for a specific city and api endpoint
-function generateUrl(endpoint, city_name, units = "imperial"){
-// endpoint =[weather,forecast,air_pollution]
+function generateUrl(endpoint, city_name, units = "imperial") {
+    // endpoint =[weather,forecast,air_pollution]
     return `https://api.openweathermap.org/data/2.5/${endpoint}?q=${city_name}&units=${units}&appid=${API_KEY}`
 }
 //adapts to different open weather api endpoints
-async function fetchCurrentWeather (city) {
+async function fetchCurrentWeather(city) {
     const response = await fetch(generateUrl("weather", city));
 
     const jsonData = await response.json();
-    console.log(jsonData)
-    if(!response.ok){
+
+    if (!response.ok) {
         console.error(jsonData)
     }
     //descructure response
-    const { coord, dt: last_updated, main: currentConditions ,name, sys:sun_data, weather, wind}=jsonData
+    const { coord, dt: last_updated, main: currentConditions, name, sys: sun_data, weather, wind } = jsonData
 
     //save the city to local storage
-    LocalStorageUtil.setItem("recents",name)
+    LocalStorageUtil.setItem("recents", name)
 
     // console.log(coord, last_updated, currentConditions, name, sun_data, weather, wind);
 
     //create current weather card with current and details
     generateCard(name, weather, currentConditions, sun_data, wind, last_updated)
-   
-    
+
+
+}
+async function fetchForecast(city) {
+    const response = await fetch(generateUrl("forecast", city));
+
+    const jsonData = await response.json();
+    console.log(jsonData)
+    if (!response.ok) {
+        console.error(jsonData)
+    }
+    //descructure response
+    const { list } = jsonData;
+    console.log(list)
+
+    let htmlString = ''
+    list.forEach((timeblock, idx) => {
+        const { dt_txt, weather, main } = timeblock;
+        console.log({ dt_txt, weather, main })
+        // every new day I want to create a new accordian item and accordian collapse group
+
+        if (dt_txt.split(' ')[1] == "00:00:00" || idx == 0) {
+            htmlString += `<li class="list-group-item d-flex justify-content-between">
+                        <h4>${dayjs(dt_txt).format('ddd')}</h4>
+                        <ul class="list-group">
+                        `
+        }
+
+        //  create list items and add it html string
+        htmlString += `<li
+                                class="row p-0 border-end-0 border-start-0 border-top d-flex justify-content-center text-lead align-items-center w-100">
+                                <small class="col-3  ">${dayjs(dt_txt).format('ha')}</small>
+                                <img src="https://openweathermap.org/img/wn/${weather[0].icon}@2x.png" alt="${weather[0].description}" class="col-2" />
+                                <div class=" d-flex  col-6 justify-content-between align-items-center gap-1 ">
+                                    <p class=" m-0 cold">${Math.floor(main.temp_min)}°</p>
+                                    <div class="bg-dark w-100 m-0 p-0 rounded-pill gradient " style="height:3px;">
+                                    </div>
+                                    <p class=" m-0 hot">${Math.floor(main.temp_max)}°</p>
+                                </div>
+                            </li>`
+
+        //on the last item on the day close the list group
+        if (dt_txt.split(' ')[1] == "21:00:00") {
+            htmlString += "</ul></li >"
+        }
+
+        //
+
+
+
+    })
+    // console.log(htmlString)
+    document.getElementById("forecast").getElementsByTagName('ul')[0].innerHTML = htmlString
+    //update the inner html of #accordian with the string
+
+    // console.log(coord, last_updated, currentConditions, name, sun_data, weather, wind);
+
+    //create current weather card with current and details
+
 }
 // render previosly search cities from localStorage
-function init(){
+function init() {
 
     fetchCurrentWeather("Anaheim")
+    fetchForecast("Anaheim")
 }
 // getCurrentWeather()
 
-function generateCard(name, weather, currentConditions, sun_data, wind, last_updated){
-    console.log({ name, weather, currentConditions, sun_data, wind, last_updated })
 
-    const iconUrl =`https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`
-    console.log(iconUrl)
+function generateCard(name, weather, currentConditions, sun_data, wind, last_updated) {
+    // console.log({ name, weather, currentConditions, sun_data, wind, last_updated })
+
+    const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`
+    // console.log(iconUrl)
     //generate current weather card
     document.getElementById("currentDay").getElementsByTagName('p')[0].textContent = dayjs().format("dddd")
     document.getElementById("currentDay").getElementsByTagName('p')[1].textContent = dayjs().format("MMMM D, YYYY")
-    document.getElementById("icon").src =iconUrl;
-    console.dir(currentWeatherCard.getElementsByTagName('img'))
+    document.getElementById("icon").src = iconUrl;
+    // console.dir(currentWeatherCard.getElementsByTagName('img'))
     document.getElementById("lastUpdate").getElementsByTagName('small')[1].textContent = dayjs.unix(last_updated).format("h:mm a")
-    currentWeatherCard.getElementsByTagName('h5')[0].textContent=name
+    currentWeatherCard.getElementsByTagName('h5')[0].textContent = name
     currentWeatherCard.getElementsByTagName('h1')[0].textContent = `${Math.floor(currentConditions.temp)}°`
     currentWeatherCard.getElementsByTagName('p')[0].textContent = `${weather[0].description}`
-    currentWeatherCard.getElementsByTagName('div')[0].innerHTML=""
+    currentWeatherCard.getElementsByTagName('div')[0].innerHTML = ""
     currentWeatherCard.getElementsByTagName('div')[0].appendChild(generateHTMLElement("p", "lead m-0", `H: ${Math.floor(currentConditions.temp_max)}°`))
     currentWeatherCard.getElementsByTagName('div')[0].appendChild(generateHTMLElement("p", "lead m-0", `L: ${Math.floor(currentConditions.temp_min)}°`))
 
     //generate sunrise + sunset card
     const sunElements = document.getElementById("sun")
-   sunElements.getElementsByTagName('p')[0].textContent = `Sunrise: ${dayjs.unix(sun_data.sunrise).format("h:mm a")}`
-    sunElements.getElementsByTagName('p')[1].textContent = `Sunset: ${dayjs.unix(sun_data.sunset).format("h:mm a") }`
+    sunElements.getElementsByTagName('p')[0].textContent = `Sunrise: ${dayjs.unix(sun_data.sunrise).format("h:mm a")}`
+    sunElements.getElementsByTagName('p')[1].textContent = `Sunset: ${dayjs.unix(sun_data.sunset).format("h:mm a")}`
 
 
     //generate details card
@@ -127,7 +186,7 @@ function generateCard(name, weather, currentConditions, sun_data, wind, last_upd
 
 
     //generate wind card
-    document.getElementById("compass").getElementsByTagName('p')[0].textContent =`${wind.speed}`
+    document.getElementById("compass").getElementsByTagName('p')[0].textContent = `${wind.speed}`
     const root = document.querySelector(':root');
 
     // set css variable
@@ -136,7 +195,7 @@ function generateCard(name, weather, currentConditions, sun_data, wind, last_upd
 }
 
 //enter classes as a space seperated list
-function generateHTMLElement(element,classes,text){
+function generateHTMLElement(element, classes, text) {
     const newEl = document.createElement(element);
     classes.split(" ").forEach(className => newEl.classList.add(className));
     newEl.textContent = text;
